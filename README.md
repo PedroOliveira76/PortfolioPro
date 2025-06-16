1232159ab6e271160a3baa1fe301180785f19d5e
Pedro Oliveira <102552855+PedroOliveira76@users.noreply.github.com>
2025-06-16 10:15:31 -0300

    Micro Changes

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-### Portfogit commit -m "first commit"lio Profssional
+### Portfolio Profissional
 
 # Criei esse portifolio para mostrar alguns projetos que irei criar e também para postar projetos antigos que vou reformular para ter um aspecto mais profissional.
 
--- src/components/Aside.tsx
+++ src/components/Aside.tsx
@@ -1,7 +1,7 @@
 import { NavLink } from 'react-router-dom'
 export function Aside() {
   return (
-    <aside className=" w-72 bg-[#0F3460] text-white border-r h-full p-4 flex flex-col items-center gap-4">
+    <aside className=" w-72 bg-[#0F3460] text-white border-r h-full p-4 flex flex-col items-center gap-4 overflow-auto">
 
       {/* Perfil  */}
       <div className=" w-full mt-16 flex flex-col justify-center items-center pointer-events-none">
@@ -10,7 +10,7 @@ export function Aside() {
         <p className="text-lg">Desenvolvedor Fullstack</p>
       </div>
 
-      <div className="text-sm mt-16 space-y-2 flex flex-col items-start justify-center  w-full ">
+      <div className="text-sm mt-2 space-y-2 flex flex-col items-start justify-center  w-full ">
 
         <h1 className="w-full text-center text-2xl ">Contatos</h1>
         <ul className="w-full h-52 flex flex-col justify-evenly">
